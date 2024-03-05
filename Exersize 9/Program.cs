6292cb2758b6c8a7c793a0aba1782e22e8f059be
Stagnet Water <145466458+Thanatoast-H@users.noreply.github.com>
2024-03-05 10:13:03 -0500

    Exersize 9

---
+++ Exersize 9/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exersize_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter width: ");
+            int width = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of rows: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int a = 0; a < width; a++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
